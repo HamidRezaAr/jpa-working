0fd8bd6d1c5bb76a0e1eff916e7e4d118b978914
hra <info.hrakbari@gmail.com>
2020-04-14 21:20:03 +0430

    Initial commit

---
+++ src/main/java/com/hra/jpa/working/jpaworking/JpaWorkingApplication.java
@@ -0,0 +1,13 @@
+package com.hra.jpa.working.jpaworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWorkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWorkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hra/jpa/working/jpaworking/JpaWorkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hra.jpa.working.jpaworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWorkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
